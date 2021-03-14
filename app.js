b226a876deebb5074c6c8f9e02dce2f495ecae03
Cricky <crickmonster@gmail.com>
2021-03-14 09:26:01 -0400

    final commit before starting frontend

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const { ADDRCONFIG } = require("dns");
 
 var stocksRouter = Require("./routes/stocks");
 var walletRouter = Require("./routes/wallet");
@@ -28,8 +29,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+//code to bootstrap wallet with $$$ goes here
+
 app.use("/api/v1/stocks", stocksRouter);
+//DELETE
+//UPDATE
+//CREATE
 app.use("/api/v1/wallet", walletRouter);
+//UPDATE
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
